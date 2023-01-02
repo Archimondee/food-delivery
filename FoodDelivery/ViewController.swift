e2435d0635fa740273e26020cbe2c30610b73d3b
Archimondee <kikchiemedia@gmail.com>
2023-01-02 10:57:23 +0700

    Initial Commit

---
+++ FoodDelivery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodDelivery
+//
+//  Created by Gilang Aditya Rahman on 02/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
